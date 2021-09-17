3540f9e03677e677497c1100069d1cfb03b87797
rafaelg <romilson.2r@gmail.com>
2021-09-17 13:04:31 -0300

    First commit

---
+++ MarvelReleases/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MarvelReleases.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
